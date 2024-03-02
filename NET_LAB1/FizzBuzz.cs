cdd763be3e6600cceedbe9545cc3f546ffa46608
Dawid Szczepaniak <dawidszczepaniak55@gmail.com>
2024-03-02 10:40:00 +0100

    Frst commit

---
+++ NET_LAB1/FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+internal class FizzBuzz
+{
+    private int maxNumber = 0;
+
+    public FizzBuzz(int maxNumber)
+    {
+        this.maxNumber = maxNumber;
+    }
+
+    public void CheckFizzBuzz()
+    {
+        if (maxNumber < 0)
+        {
+            Console.WriteLine("ERROR: maxNumber musi byc wiekszy od 0");
+            return;
+        }
+
+        string result = "";
+        for (int i = 1; i <= maxNumber; i++)
+        {
+            if (i % 3 == 0)
+            {
+                result = result + "Fizz";
+            }
+
+            if (i % 5 == 0)
+            {
+                result = result + "Buzz";
+            }
+
+            if (result.Length == 0) result = "" + i;
+            Console.WriteLine(result);
+            result = "";
+        }
+    }
+}
---
+++ NET_LAB1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NET_LAB1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz(30);
+            fizzBuzz.CheckFizzBuzz();
+        }
+    }
+}
