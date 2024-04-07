961976b31987d1898df241608ffda09da2633b46
Dawid Szczepaniak <dawidszczepaniak55@gmail.com>
2024-04-07 17:41:00 +0200

    changes

--- LAB3/LAB3/ClientAPI.cs
+++ LAB3/LAB3/ClientAPI.cs
@@ -6,23 +6,6 @@ public class ClientAPI : Singleton<ClientAPI>
 
     private const string BASE_URL = "https://www.freetogame.com/api";
     
-    public async Task Call<T>(string call, Action<T, HttpResponseMessage> OnSuccessful = null , Action OnFailure = null)
-    {
-        try
-        {
-            using HttpResponseMessage response = await client.GetAsync(BASE_URL +  call);
-            string responseBody = await response.Content.ReadAsStringAsync();
-            T t = JsonSerializer.Deserialize<T>(responseBody);
-            OnSuccessful?.Invoke(t, response);
-        }
-        catch (HttpRequestException e)
-        {
-            Console.WriteLine("\nException Caught!");
-            Console.WriteLine("Message :{0} ", e.Message);
-            OnFailure?.Invoke();
-        }
-    }
-    
     public async Task Call<T>(string call, Action<T, HttpResponseMessage> OnSuccessful = null, Action OnFailure = null, CancellationToken cancellationToken = default)
     {
         try
--- LAB3/LAB3/Program.cs
+++ LAB3/LAB3/Program.cs
@@ -6,27 +6,6 @@ class Program
 {
     static void Main()
     {
-        ClientAPI client = ClientAPI.Instance;
-        
-        client.Call<List<Game>>("/games",
-            OnSuccessful: async (body, response) =>
-            {
-                if (response.IsSuccessStatusCode)
-                {
-                    for (int i = 0; i < body.Count; i++)
-                    {
-                        Console.WriteLine(body[i]);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("RESPONSE CODE: " + response.StatusCode);
-                }
-            },
-            OnFailure: () =>
-            {
-                Console.WriteLine("Conection failure");
-            }).Wait();
     }
-    
+
 }
