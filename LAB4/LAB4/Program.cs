f4dbbe83e357ad95b0e988fbae2c669a088a3b09
Dawid Szczepaniak <dawidszczepaniak55@gmail.com>
2024-04-22 18:06:29 +0200

    Update Program.cs

--- LAB4/LAB4/Program.cs
+++ LAB4/LAB4/Program.cs
@@ -9,21 +9,29 @@ namespace LAB4
             int[,] a =  MatrixOperations.GenerateMatrix(1000, 1);
             int[,] b =  MatrixOperations.GenerateMatrix(1000, 2);
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            int[,] result = MatrixOperations.Multiplication(a, b);
+            MatrixOperations.Multiplication(a, b);
             watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine($"Zwykle: {elapsedMs} ms");
-            watch = System.Diagnostics.Stopwatch.StartNew();
-            int[,] resultThread = MatrixOperations.Multiplication(a, b, 10);
-            watch.Stop();
-            elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine($"Watki: {elapsedMs} ms");
-            
+            long elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"Watek [1]: {elapsedMs} ms");
+            for (int i = 2; i <= 16; i++)
+            {
+                CalculateMatrix(a, b, i);
+            }
+
             /*Console.WriteLine(MatrixOperations.MatrixToString(a));
             Console.WriteLine(MatrixOperations.MatrixToString(b));
             Console.WriteLine(MatrixOperations.MatrixToString(result));
             Console.WriteLine(MatrixOperations.MatrixToString(resultThread));*/
 
+        }
+
+        private static void CalculateMatrix(int[,] a, int[,] b, int threads)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            MatrixOperations.Multiplication(a, b, threads);
+            watch.Stop();
+            long elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"Watki [{threads}]: {elapsedMs} ms");
         }
     }
 }
